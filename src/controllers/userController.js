9037ae72b7dfaf6099c51f7e4a4738fcd8abec4a
Gabriel69 <gabrielcortes69x@gmail.com>
2025-02-05 22:07:52 -0500

    Se agrego actualizar usuario

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -41,3 +41,13 @@ const getUserById = async (req, res) => {
         res.status(500).json({ message: 'Error al obtener usuario', error: error.message });
     }
 };
+
+const updateUser = async (req, res) => {
+    try {
+        const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, { new: true }).select('-password');
+        if (!updatedUser) return res.status(404).json({ message: "Usuario no encontrado" });
+        res.json({ message: 'Usuario actualizado correctamente', user: updatedUser });
+    } catch (error) {
+        res.status(500).json({ message: 'Error al actualizar usuario', error: error.message });
+    }
+};
