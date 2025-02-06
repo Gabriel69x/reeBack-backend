aeefb24005217838ab184726d55a6f36934c724c
Gabriel69 <gabrielcortes69x@gmail.com>
2025-02-05 21:49:16 -0500

    Se agrego resgitrar a el usuario con su validacion y encriptacion de contraseña

---
+++ src/controllers/userController.js
@@ -0,0 +1,24 @@
+
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+
+const registerUser = async (req, res) => {
+    try {
+        const { nombre, email, password, role } = req.body;
+
+        // Verificar si el usuario ya existe
+        let user = await User.findOne({ email });
+        if (user) return res.status(400).json({ message: 'El usuario ya existe' });
+
+        // Encriptar la contraseña
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        // Crear usuario nuevo
+        user = new User({ nombre, email, password: hashedPassword, role });
+        await user.save();
+        res.status(201).json({ message: 'Usuario registrado correctamente', user });
+    } catch (error) {
+        res.status(500).json({ message: 'Error al registrar usuario', error: error.message });
+    }
+};
